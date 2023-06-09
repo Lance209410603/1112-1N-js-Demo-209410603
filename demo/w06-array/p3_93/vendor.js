d64b73a4bb08aa55725f2030ba36325124ba7c91
Lance209410603 <209410603@gms.tku.edu.tw>
2023-06-09 21:13:43 +0800

    123

--- demo/w06-array/p3_93/vendor.js
+++
@@ -1,4 +0,0 @@
-const userInput = document.getElementById('input-number');
-const cBtn = document.getElementById('btn-c');
-const fBtn = document.getElementById('btn-f');
-const currentCalculationOutput = document.getElementById('current-calculation');
